2822df08c5958917d38e4c7728abd38d043133ca
smile437 <smile437@gmail.com>
2015-06-01 20:07:19 +0300

    add
    add

---
+++ MvcApplication1/MvcApplication1/Controllers/HomeController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.UI.WebControls;
+using MvcApplication1.Models;
+
+namespace MvcApplication1.Controllers
+{
+    public class HomeController : Controller
+    {
+        [AllowAnonymous]
+        public ActionResult Index()
+        {
+            ViewBag.Message = "Learn a foreign language easier than ever.";
+
+            return View();
+        }
+        [AllowAnonymous]
+        public ActionResult About()
+        {
+            return View();
+        }
+        [AllowAnonymous]
+        public ActionResult Contact()
+        {
+            return View();
+        }
+        public ActionResult StartLearn(int p=0)
+        {
+            /*List<UserProfile> users = new List<UserProfile>();
+            string user = "";
+            using (UsersContext db = new UsersContext())
+            {
+                user = db.Database.ExecuteSqlCommand("SELECT Name FROM UserProfile WHERE UserId=38").ToString();
+                users = db.UserProfiles.Where(u => u.UserId == 37).ToList();
+                /*Дописать свитч на запросы*/
+            //}
+           return View();
+        }
+        public ActionResult Chat() 
+        {
+            return View();
+        }
+    }
+}
